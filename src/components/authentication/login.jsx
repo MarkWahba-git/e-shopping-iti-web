44396efad3ef11135467d5bf327ee3a3a13414ae
Andrew <andrewnagynemr@gmail.com>
2020-05-08 21:09:07 +0200

    login modifications

--- src/components/authentication/login.jsx
+++ src/components/authentication/login.jsx
@@ -1,26 +1,48 @@
 import React, { Component } from "react";
 class LogIn extends Component {
-  state = {};
+  state = {
+    account: {
+      email: "",
+      password: "",
+    },
+    errors: {},
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+    console.log(this.state.account);
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("logged in to the server");
+  };
+
   render() {
+    const { account } = this.state;
     return (
       <div className="container">
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="exampleInputEmail1">Email address</label>
             <input
+              onChange={this.handleChange}
+              value={account.name}
               type="email"
+              name="email"
               className="form-control"
               id="exampleInputEmail1"
-              aria-describedby="emailHelp"
             />
-            <small id="emailHelp" className="form-text text-muted">
-              We'll never share your email with anyone else.
-            </small>
           </div>
           <div className="form-group">
-            <label htmlfor="exampleInputPassword1">Password</label>
+            <label htmlFor="exampleInputPassword1">Password</label>
             <input
+              onChange={this.handleChange}
+              value={account.password}
               type="password"
+              name="password"
               className="form-control"
               id="exampleInputPassword1"
             />
